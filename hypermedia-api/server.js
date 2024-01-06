3977933e09d93a95a16e38f851507f4cb9c60050
yiannis <107484245+DimYiannis@users.noreply.github.com>
2024-01-06 21:22:22 +0200

    Update server.js

--- hypermedia-api/server.js
+++ hypermedia-api/server.js
@@ -9,6 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.send('<h2> Welcome to the Node Hypermedia </h2>');
