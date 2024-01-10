2e2e23f8cbb38aeadaac7fe9c1e23d696c565212
yiannis <107484245+DimYiannis@users.noreply.github.com>
2024-01-10 17:44:54 +0200

    Update index.js

--- ws-server/index.js
+++ ws-server/index.js
@@ -8,7 +8,16 @@ server.on("connection", (socket)=> {
     console.log(`client connected`);
 
     socket.on("message", (data) => {
+
         const sent_data = JSON.parse(data);
         console.log(sent_data.chat_message);
+
+        // i placed the id of the element i want the data to be shown 
+        //in the frontend and used hx-oob-swap to achieve this
+        socket.send(`
+            <div id="chat_box" hx-swap-oob="true"> 
+                <h3>${sent_data.chat_message}</h3>
+            </div>
+        `)
     })
 })
