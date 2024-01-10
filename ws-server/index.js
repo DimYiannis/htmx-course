7e595166cc255e6a6fcc11c679f40e8446d8d86d
yiannis <107484245+DimYiannis@users.noreply.github.com>
2024-01-10 16:10:23 +0200

    websocket send data to the server

--- ws-server/index.js
+++ ws-server/index.js
@@ -6,4 +6,9 @@ const server = new WebSocketServer({
 
 server.on("connection", (socket)=> {
     console.log(`client connected`);
+
+    socket.on("message", (data) => {
+        const sent_data = JSON.parse(data);
+        console.log(sent_data.chat_message);
+    })
 })
