14800dbcb6e705affd767275397fa6741e618040
yiannis <107484245+DimYiannis@users.noreply.github.com>
2024-01-11 11:15:40 +0200

    Update index.js

--- ws-server/index.js
+++ ws-server/index.js
@@ -1,23 +1,24 @@
-const {WebSocketServer} = require("ws");
+const { WebSocketServer } = require("ws");
 
 const server = new WebSocketServer({
-    port:5000
+  port: 5000,
 });
 
-server.on("connection", (socket)=> {
-    console.log(`client connected`);
+server.on("connection", (socket) => {
+  console.log(`client connected`);
 
-    socket.on("message", (data) => {
+  socket.on("message", (data) => {
+    const sent_data = JSON.parse(data);
+    console.log(sent_data.chat_message);
 
-        const sent_data = JSON.parse(data);
-        console.log(sent_data.chat_message);
-
-        // i placed the id of the element i want the data to be shown 
-        //in the frontend and used hx-oob-swap to achieve this
-        socket.send(`
-            <div id="chat_box" hx-swap-oob="true"> 
-                <h3>${sent_data.chat_message}</h3>
-            </div>
-        `)
-    })
-})
+    setInterval(() => {
+      // i placed the id of the element i want the data to be shown
+      //in the frontend and used hx-oob-swap to achieve this
+      socket.send(`
+        <div id="chat_box" hx-swap-oob="beforeend"> 
+            <h3>${sent_data.chat_message}</h3>
+        </div>
+    `);
+    }, 1000);
+  });
+});
